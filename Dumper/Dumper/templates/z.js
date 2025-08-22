d3a52bca28ee7fff2a67b1acda115f648616b49e
jumpyapple <70656949+jumpyapple@users.noreply.github.com>
2025-08-22 07:19:16 -0400

    feat(Dumper): allow click to copy method name
    Fix name of an array does not have the index.

--- Dumper/Dumper/templates/z.js
+++ Dumper/Dumper/templates/z.js
@@ -1,24 +1,47 @@
-const dt = "data-theme";
-let w = window;
-let d = document;
-w.addEventListener("load", () => {
-	let h = document.getElementsByTagName("html")[0];
-	if (w.matchMedia && w.matchMedia('(prefers-color-scheme: dark)').matches) {
-		h.setAttribute(dt, "dark");
-	} else {
-		h.setAttribute(dt, "light");
+async function copyTextToClipboard(text) {
+	try {
+		await navigator.clipboard.writeText(text);
+	} catch (err) {
+		console.error("failed to copy text: ", err);
 	}
+}
+
+window.addEventListener("load", () => {
+	// Check system preference and switch to that theme.
+	let html_tag = document.getElementsByTagName("html")[0];
+	if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		html_tag.setAttribute("data-theme", "dark");
+	} else {
+		html_tag.setAttribute("data-theme", "light");
+	}
+
+	// Setup theme switching.
 	document.getElementById("dark-btn").addEventListener("click", (e) => {
 		e.preventDefault();
-		h.setAttribute(dt, "dark");
+		html_tag.setAttribute("data-theme", "dark");
 	});
 	document.getElementById("light-btn").addEventListener("click", (e) => {
 		e.preventDefault();
-		h.setAttribute(dt, "light");
+		html_tag.setAttribute("data-theme", "light");
 	});
-	const sp = new URLSearchParams(w.location.search);
-	if (sp.has("n")) {
-		let e = d.getElementById("vn");
-		e.innerHTML = sp.get("n");
+
+	// Allow the previous page to pass the name to this page.
+	const searchParams = new URLSearchParams(window.location.search);
+	if (searchParams.has("n")) {
+		document.getElementById("vn").innerHTML = sp.get("n");
 	}
+
+	// Allow copying a method's name.
+	var methodElements = document.querySelectorAll("#method div ul li");
+	methodElements.forEach((element) => {
+		element.addEventListener("click", (e) => {
+			// Parse for the method name.
+			const tokens = element.innerText.split("'");
+			if (tokens.length >= 2) {
+				copyTextToClipboard(tokens[1]);
+			}
+		});
+	});
 });
+
+
